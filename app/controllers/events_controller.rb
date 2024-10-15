b0b299e1fd469d2a45d86e75869a431aa09427b2
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-15 11:58:32 +0100

    Add #new, #create and #show method to EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,30 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(perm_event_params)
+
+    @event.build_creator(id: current_user.id)
+
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def perm_event_params
+    params.require(:event).permit(:name, :date, :location)
+  end
 end
