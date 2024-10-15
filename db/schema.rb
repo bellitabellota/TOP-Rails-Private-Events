6e806c5ccf4544ac8ab487ced7baab7654138023
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-15 09:28:10 +0100

    Migrate to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_143956) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_15_080052) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_143956) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
