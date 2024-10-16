eaa7d1ebd5cfce4663a1f5a058851304721b1d9f
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-16 10:41:15 +0100

    DB:rollback and add missing to_table to migration

--- db/migrate/20241015154415_create_event_attendings.rb
+++ db/migrate/20241015154415_create_event_attendings.rb
@@ -1,8 +1,8 @@
 class CreateEventAttendings < ActiveRecord::Migration[7.2]
   def change
     create_table :event_attendings do |t|
-      t.references :attended_event, null: false, foreign_key: true
-      t.references :event_attendee, null: false, foreign_key: true
+      t.references :attended_event, null: false, foreign_key: { to_table: :events }
+      t.references :event_attendee, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
