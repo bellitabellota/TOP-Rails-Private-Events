d2f7f5480c10c963ffb185df938d65b22b5d767b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-16 14:47:49 +0100

    Display attended_events on User#show view using Active Record Query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def show
     @events_created = Event.where("creator_id = ?", current_user.id)
-    @upcoming_events = current_user.attended_events.map { |event| event if event.date.to_date > Date.current }.compact
-    @past_events = current_user.attended_events.map { |event| event if event.date.to_date < Date.current }.compact
+
+    @attended_events = Event.joins(:event_attendings).where("event_attendings.event_attendee_id": current_user.id)
+
+    # @upcoming_events = current_user.attended_events.map { |event| event if event.date.to_date > Date.current }.compact
+    # @past_events = current_user.attended_events.map { |event| event if event.date.to_date < Date.current }.compact
   end
 end
