9a7a8b441d20bd747760b40b6454f746df90e451
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-16 13:00:02 +0100

    Separate 'Attended_events' into past and upcoming events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @events_created = Event.where("creator_id = ?", current_user.id)
+    @upcoming_events = current_user.attended_events.map { |event| event if event.date.to_date > Date.current }.compact
+    @past_events = current_user.attended_events.map { |event| event if event.date.to_date < Date.current }.compact
   end
 end
