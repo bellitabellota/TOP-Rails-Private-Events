1611d80b24c49522807ff5fb3056b9048c179d75
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-16 11:00:24 +0100

    Add EventAttendings#create

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,2 +1,19 @@
 class EventAttendingsController < ApplicationController
+  def create
+    @event_attending = EventAttending.new(perm_event_attending_params)
+
+    if @event_attending.save
+      flash[:success] = "Your participation at this event is confirmed."
+      redirect_to event_path(:attended_event_id)
+    else
+      flash.now[:error].now = "Your request was not correctly process. Please try again or contact the support."
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def perm_event_attending_params
+    params.require(:event_attending).permit(:attended_event_id, :event_attendee_id)
+  end
 end
