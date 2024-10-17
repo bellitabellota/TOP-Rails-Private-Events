f895b2515f864f1b5d486ace70fd3678876c423c
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-17 16:09:51 +0100

    Separate 'Attended_events' into past and upcoming events with Active Record Query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     @events_created = Event.where("creator_id = ?", current_user.id)
 
     @attended_events = Event.joins(:event_attendings).where("event_attendings.event_attendee_id": current_user.id)
+    @upcoming_events = Event.where("date >= ?", Date.current).joins(:event_attendings).where("event_attendings.event_attendee_id": current_user.id)
+    @past_events = Event.where("date < ?", Date.current).joins(:event_attendings).where("event_attendings.event_attendee_id": current_user.id)
 
     # @upcoming_events = current_user.attended_events.map { |event| event if event.date.to_date > Date.current }.compact
     # @past_events = current_user.attended_events.map { |event| event if event.date.to_date < Date.current }.compact
