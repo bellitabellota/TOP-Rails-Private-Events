316e0675352456787c24fd674e761f72515d03c6
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-17 16:44:35 +0100

    Separate Upcoming/Past Events on events#index with class method in model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
 
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
+
+  def self.upcoming
+    Event.where("date >= ?", Date.current)
+  end
+
+  def self.past
+    Event.where("date < ?", Date.current)
+  end
 end
