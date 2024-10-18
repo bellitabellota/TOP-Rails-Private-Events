68fc50d57bcdc11772098142513fc5676084f3ed
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-18 15:04:52 +0100

    Add depedent destroy to Event model so events with attendees can be destroyed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
-  has_many :event_attendings, foreign_key: :attended_event_id
-  has_many :attendees, through: :event_attendings, source: :event_attendee
+  has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
+  has_many :attendees, through: :event_attendings, source: :event_attendee, dependent: :destroy
 
   # def self.upcoming
   #   Event.where("date >= ?", Date.current)
