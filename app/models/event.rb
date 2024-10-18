04b90543493c536417f5828fd9c9eadb754b7d90
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-10-18 10:11:52 +0100

    Refactor to use scopes to separate past and upcoming events on events#index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,15 @@ class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
 
-  def self.upcoming
-    Event.where("date >= ?", Date.current)
-  end
+  # def self.upcoming
+  #   Event.where("date >= ?", Date.current)
+  # end
 
-  def self.past
-    Event.where("date < ?", Date.current)
-  end
+  # def self.past
+  #   Event.where("date < ?", Date.current)
+  # end
+
+  scope :upcoming, -> { where("date >= ?", DateTime.current) }
+
+  scope :past, -> { where("date < ?", DateTime.current) }
 end
